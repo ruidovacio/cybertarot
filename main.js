145e308d1ec0c35e7b3d01a46feae3755f7f07d7
ruidovacio <nikkolazhito@hotmail.com>
2023-02-23 11:53:15 -0300

    testing effects on cards

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const url = new URL(url_string);
 const parametro = url.searchParams.get("spread");
 console.log(parametro);
 
-cargarCartas();
+cargarCartas()
 
 async function cargarCartas() {
     const response = await fetch('tarot-images.json');
@@ -14,6 +14,8 @@ async function cargarCartas() {
     const baraja = mezclarCartas(tarot.cards);
     console.log(baraja);
     hacerTabla(baraja, parametro);
+    //interactuar(baraja, parametro);
+    interactuar(baraja);
 }
 
 async function hacerTabla(baraja, tirada) {
@@ -66,22 +68,74 @@ async function hacerTabla(baraja, tirada) {
 }
 
 function displayCartas(barajaTabulada, cantidadFilas) {
+    count = 0;
     for (i = 0; i < cantidadFilas; i++) {
         //se crean tantos rows como cantidad de filas tenga la barja luego de la tabulación
         const row = document.createElement('div');
         row.classList.add('row');
         row.classList.add('justify-content-center');
-        console.log(barajaTabulada[i]);
         for (let j = 0; j < barajaTabulada[i].length; j++) {
             const col = document.createElement('div');
+            const cartabloque = document.createElement('div');
+            cartabloque.classList.add('cartabloque');
+            const overlay = document.createElement('div');
+            overlay.classList.add('overlay');
+            const texto = document.createElement('div');
+            texto.classList.add('text');
+            texto.innerHTML = "<h2>" + barajaTabulada[i][j].name + "</h2><p>" + barajaTabulada[i][j].fortune_telling + "</p>";
             const imagen = document.createElement('img');
             imagen.src = "cards/" + barajaTabulada[i][j].img; //el contador [i][j], es el índice en la fila elegida
-            col.appendChild(imagen);
-            col.classList.add('col-auto');
+            imagen.setAttribute("id", count);
+            console.log(count);
+            count = count + 1;
+            cartabloque.appendChild(imagen);
+            cartabloque.appendChild(overlay);
+            overlay.appendChild(texto);
+            col.appendChild(cartabloque);
+            col.classList.add('col-6');
+            col.classList.add('col-md-4');
             row.appendChild(col);
         }
         container.appendChild(row);
     }
+}
+
+// async function getElementsByIds(ids) {
+//     var idList = await ids.split(" ");
+//     var results = [], item;
+//     for (var i = 0; i < idList.length; i++) {
+//         item = document.getElementById(idList[i]);
+//         if (item) {
+//             results.push(item);
+//         }
+//     }
+//     console.log(results);
+// }
+
+async function interactuar(baraja){
+    document.addEventListener('click', (e) =>
+    {
+        let elementId = e.target.id;
+        if (elementId != '') {
+            console.log(baraja[elementId].name);
+        }
+        else {
+            console.log("nada");
+        }
+    })
+    //elemento1 = await document.getElementById("0");
+    //elemento2 = await document.getElementById("1");
+    //elemento3 = await document.getElementById("2");
+    // }
+    // elemento1.onclick = function(){
+    //     console.log(baraja[0].name);
+    // }
+    // elemento2.onclick = function(){
+    //     console.log(baraja[1].name);
+    // }
+    // elemento3.onclick = function(){
+    //     console.log(baraja[2].name);
+    // }
 }
 
 function mezclarCartas(mazoElegido) {
